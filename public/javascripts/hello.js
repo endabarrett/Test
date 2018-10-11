506ac8eba8e33c25a8234d26de7360c1426949df
ENDA BARRETT( 6-10-2016 10-00 <danu7_be1@danu7>
2018-10-11 14:00:06 +0100

    First commit

---
+++ public/javascripts/hello.js
@@ -0,0 +1,76 @@
+alert("Hello World!");
+
+
+
+
+
+
+/*var myAge = 26;
+
+function incrementAge(myAge)
+{
+    myAge++;
+
+    console.log("My age in function ", myAge);
+}
+//incrementAge(myAge);
+console.log("My age outside function ", myAge);
+*/
+
+/*
+var a = 4;
+var b = 2;
+var c = 5;
+var d = 3;
+
+var eq1 = (a+b)*(c+d); //48
+var eq2 = a+b*c+d; // 17
+var eq3 = (d*(a-b))+c; // 11
+var eq4 = d*a-b+c; // 15
+
+alert(eq1);
+alert(eq2);
+alert(eq3);
+alert(eq4);
+
+
+var salary = prompt("Please enter your salary", "Type here");
+
+if(parseInt(salary) > 50000)
+{
+    window.alert("You are wealthy");
+}
+else
+    window.alert("You are poor");
+
+var maxLoan = parseInt(salary)*3.5;
+window.alert("You can borrow " + maxLoan);
+*/
+//Repeat while i is still less than 10
+/*for(var i=0; i<10; i++)
+{
+     if(i>7 || i<9)
+     {
+         alert(i);
+         break;
+     }
+}*/
+
+
+/*
+function incrementAge(myAge)
+{
+    myAge++;
+    simple = 0;
+    simple++;
+    return myAge;
+}
+
+incAge = incrementAge(26);
+alert(simple);
+alert("Incremented age is " + incAge);
+
+
+
+*/
+
